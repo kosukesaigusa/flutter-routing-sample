4612ddeca3f487e36410b96f999fde54036d085e
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-06-01 22:41:20 +0900

    feat: define get repo api response data

---
+++ lib/models/api/get_repo_response/get_repo_response.dart
@@ -0,0 +1,27 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../repo/repo.dart';
+import '../base_response_data/base_response_data.dart';
+
+part 'get_repo_response.freezed.dart';
+part 'get_repo_response.g.dart';
+
+@freezed
+class GetRepoResponse with _$GetRepoResponse {
+  const factory GetRepoResponse({
+    @Default(true) bool success,
+    @Default('') String message,
+    required Repo repo,
+  }) = _GetRepoResponse;
+
+  factory GetRepoResponse.fromJson(Map<String, dynamic> json) => _$GetRepoResponseFromJson(json);
+
+  factory GetRepoResponse.fromBaseResponseData(BaseResponseData baseResponseData) =>
+      GetRepoResponse.fromJson(
+        <String, dynamic>{
+          'success': baseResponseData.success,
+          'message': baseResponseData.message,
+          ...baseResponseData.data,
+        },
+      );
+}
---
+++ lib/models/api/get_repo_response/get_repo_response.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'get_repo_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_GetRepoResponse _$$_GetRepoResponseFromJson(Map<String, dynamic> json) =>
+    _$_GetRepoResponse(
+      success: json['success'] as bool? ?? true,
+      message: json['message'] as String? ?? '',
+      repo: Repo.fromJson(json['repo'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$$_GetRepoResponseToJson(_$_GetRepoResponse instance) =>
+    <String, dynamic>{
+      'success': instance.success,
+      'message': instance.message,
+      'repo': instance.repo,
+    };
